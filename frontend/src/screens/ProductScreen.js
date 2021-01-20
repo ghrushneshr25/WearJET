4b5525180b8e5ad2a3f8235bee9f77cc3329d2f4
Ghrushnesh Rathod <ghrushneshr24@gmail.com>
2021-01-20 11:32:59 -0800

    Add to cart beginning

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,14 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  Form,
+} from "react-bootstrap";
 import Rating from "../components/Rating";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
@@ -8,18 +16,21 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { listProductDetails } from "../actions/productActions";
 
-const ProductScreen = ({ match }) => {
-
-  const [qty, setQty] = useState(0)
+const ProductScreen = ({ history, match }) => {
+  const [qty, setQty] = useState(0);
 
   const dispatch = useDispatch();
 
-  const productDetails = useSelector((state) => state.productDetails)
+  const productDetails = useSelector((state) => state.productDetails);
   const { loading, error, product } = productDetails;
 
   useEffect(() => {
     dispatch(listProductDetails(match.params.id));
   }, [dispatch, match]);
+
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${qty}`);
+  };
 
   return (
     <>
@@ -27,16 +38,16 @@ const ProductScreen = ({ match }) => {
         Back
       </Link>
 
-      { loading ? (
-          <Loader />
-        ) : error ? (
-          <Message variant="danger"> {error} </Message>
-        ) : (
-          <Row>
-            <Col md={4}>
-              <Image src={product.image} alt={product.name} fluid />
-            </Col>
-            <Col md={5}>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger"> {error} </Message>
+      ) : (
+        <Row>
+          <Col md={4}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={5}>
             <ListGroup variant="flush">
               <ListGroup.Item>
                 <h3>{product.name}</h3>
@@ -76,16 +87,26 @@ const ProductScreen = ({ match }) => {
                 {product.countInStock > 0 && (
                   <ListGroup.Item>
                     <Row>
-                      <Col>Qty</Col>
+                      <Col>Qty:</Col>
                       <Col>
-                        <Form.Control as='select' value={qty} onChange={(=> 
-                        setQty(e.))}
+                        <Form.Control
+                          as="select"
+                          value={qty}
+                          onChange={(e) => setQty(e.target.value)}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
                       </Col>
                     </Row>
                   </ListGroup.Item>
                 )}
                 <ListGroup.Item>
                   <Button
+                    onClick={addToCartHandler}
                     className="btn-block"
                     type="button"
                     disabled={product.countInStock === 0}
@@ -97,8 +118,7 @@ const ProductScreen = ({ match }) => {
             </Card>
           </Col>
         </Row>
-        )
-      }
+      )}
     </>
   );
 };
